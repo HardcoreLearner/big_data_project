d6b3a591facff292e1e8e233df5feddd451a0cea
HardcoreLearner <MoBah@protonmail.com>
2025-05-12 21:52:33 +0000

    projet done

--- src/feature_extraction.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
-def extract_bow_features(texts):
-    vectorizer = CountVectorizer()
-    return vectorizer.fit_transform(texts)
-
-def extract_tfidf_features(texts):
-    vectorizer = TfidfVectorizer()
-    return vectorizer.fit_transform(texts)
